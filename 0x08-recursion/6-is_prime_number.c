18cf322e93c0430c3d65d763e4880ec8adfee59b
Nyambi Fumani <fumaniglen@gmail.com>
2022-11-02 13:14:49 -0700

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int find_multipliers(int n, int i)
 	if (n % 1 == 0)
 		return (0);
 	else
-		return (find_multipliers(n, i + 1));
+		return (find_multipliers(n, i - 1));
 }
 /**
  * is_prime_number - check if n is prime
@@ -23,6 +23,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else
-		return (find_multipliers(n, 2));
+	else if (find_multipliers(n, n / 2 > 0))
+		return (1);
+	return (0);
 }
