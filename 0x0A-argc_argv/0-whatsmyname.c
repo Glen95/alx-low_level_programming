a919d55608b408ede208b29c07095983bec5f6b8
Glen95 <fumaniglen@gmail.com>
2022-11-04 15:55:21 +0200

    Create 0-whatsmyname.c
    Argc,Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name and new line
+ * @argc: number of argumrnts typed
+ * @argv: array pointing to arguments
+ * Return: 0
+*/
+int main(int argc, char argv[])
+{
+    argc = 0;
+
+    printf("%s\n", argv[argc]);
+    return (0);
+}
