ccc8db6e9c66d2fdfa9fa1b3793c6881f26b0c47
Nyambi Fumani <fumaniglen@gmail.com>
2022-11-04 11:41:24 -0700

    Argc,Argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,14 +8,13 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
 	{
-		int i;
-
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-
-		return (0);
+		printf("%s\n", argv[i]);
 	}
-}
+
+	return (0);
+}		
