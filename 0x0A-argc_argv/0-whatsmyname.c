8a84db69ff735526b5d68c50fc73d8d4385cdf5a
Nyambi Fumani <fumaniglen@gmail.com>
2022-11-04 07:20:13 -0700

    Argv,Argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include "main.h"
 /**
  * main - prints its name and new line
- * @argc: number of argumrnts typed
+ * @argc: number of arguments typed
  * @argv: array pointing to arguments
  * Return: 0
-*/
-int main(int argc, char argv[])
+ */
+int main(int argc, char *argv[])
 {
-    argc = 0;
+	argc = 0;
 
-    printf("%s\n", argv[argc]);
-    return (0);
+	print("%s\n", argv[argc]);
+	return (0);
 }
